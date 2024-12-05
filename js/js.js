017f08d16898290d32918d6a0d046f04d5316651
vacok54 <you@example.com>
2024-12-05 07:48:08 +0300

    дз сделано

--- js/js.js
+++ js/js.js
@@ -1,8 +1,34 @@
-console.log('Hello world') //Выводим текст в консоль
+//console.log('Hello world') //Выводим текст в консоль
 
-    let myName = 'Deni';
-    let Username = 'Vacok';
+    //let myName = 'Deni';
+    //let Username = 'Vacok';
 
-    let 2World = zemlya;
-    let myaddres = 'grozny';
+    //let 2World = zemlya;
+    //let myaddres = 'grozny';
     
+    //let number = '28.02.1999'; //string
+    //let pi = 3.14; //number
+
+    //alert(`my name is ${myName}`)
+    //alert(`my username ${Username}`)
+
+    //const name = 'Deni';
+    //name = 'NeDeni';//const нельзя изменить
+    //console.log(name)
+
+    //const name = {
+    //    user: 15
+    //}                 Менять можно только свойства константы если создан объект
+    //name.user = 20
+    //console.log(name.user)
+
+    //let name = prompt( `как вас зовут?`);
+    //let adress = prompt(`Где вы живете?`)
+    //let phone = prompt(`Ваш номер телефона?`)
+
+    //console.log(`Студент ${name}, Адрес ${adress}, Телефон ${phone}`)]
+
+    //let rate = 104.50
+    //let ruble = 1050
+    //let dollars = ruble / rate
+    //console.log(`На ${ruble} можно купить ${Math.floor(dollars)} Долларов))`)
